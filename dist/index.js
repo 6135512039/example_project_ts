35f7219c23e6eb4196857e04e33bcf5164f58877
Kongkom Sumlee <kongkom.sumlee@lotuss.com>
2023-03-14 15:49:57 +0700

    create get method show all user

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+const users = [
+    {
+        id: '1',
+        name: "Tang",
+        role: "Mobile Developer"
+    },
+    {
+        id: '2',
+        name: "Ong",
+        role: "Package QA"
+    },
+    {
+        id: '3',
+        name: "Sing",
+        role: "Package QA"
+    },
+];
+function getAllUsers(req, res) {
+    res.json(users);
+}
+app.get("/user/v1/users", getAllUsers);
 app.listen(3000, () => {
-    console.log("Running on PORT 3000");
+    console.log("Running on Server PORT 3000");
 });
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,35 @@ import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
-});
+interface User {
+    id: string;
+    name: string;
+    role: string;
+}
+
+const users: User[] = [
+    {
+        id: '1',
+        name: "Tang",
+        role: "Mobile Developer"
+    },
+    {
+        id: '2',
+        name: "Ong",
+        role: "Package QA"
+    },
+    {
+        id: '3',
+        name: "Sing",
+        role: "Package QA"
+    },
+];
+
+function getAllUsers(req: Request, res: Response) {
+    res.json(users);
+}
+
+app.get("/user/v1/users", getAllUsers);
 
 app.listen(3000, () => {
     console.log("Running on Server PORT 3000");
