1d6b8931b4c4b507213ba4b9f9be3b3a02fee3dd
Kongkom Sumlee <kongkom.sumlee@lotuss.com>
2023-03-14 16:57:13 +0700

    create post method show response hello userId

--- dist/index.js
+++ dist/index.js
@@ -5,19 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const PORT = 3000;
 const users = [
     {
-        id: '1',
+        id: 1,
         name: "Tang",
         role: "Mobile Developer"
     },
     {
-        id: '2',
+        id: 2,
         name: "Ong",
         role: "Package QA"
     },
     {
-        id: '3',
+        id: 3,
         name: "Sing",
         role: "Package QA"
     },
@@ -25,7 +26,12 @@ const users = [
 function getAllUsers(req, res) {
     res.json(users);
 }
+function getUserById(req, res) {
+    const userId = req.params.userId;
+    res.send(`hello ${userId}`);
+}
 app.get("/user/v1/users", getAllUsers);
-app.listen(3000, () => {
-    console.log("Running on Server PORT 3000");
+app.post("/user/v1/users/:userId", getUserById);
+app.listen(PORT, () => {
+    console.log(`Running on Server PORT ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
+const PORT: number = 3000;
 
 interface User {
-    id: string;
+    id: number;
     name: string;
     role: string;
 }
 
 const users: User[] = [
     {
-        id: '1',
+        id: 1,
         name: "Tang",
         role: "Mobile Developer"
     },
     {
-        id: '2',
+        id: 2,
         name: "Ong",
         role: "Package QA"
     },
     {
-        id: '3',
+        id: 3,
         name: "Sing",
         role: "Package QA"
     },
@@ -30,8 +31,14 @@ function getAllUsers(req: Request, res: Response) {
     res.json(users);
 }
 
-app.get("/user/v1/users", getAllUsers);
+function getUserById(req: Request, res: Response) {
+    const userId = req.params.userId;
+    res.send(`hello ${userId}`);
+}
 
-app.listen(3000, () => {
-    console.log("Running on Server PORT 3000");
+app.get("/user/v1/users", getAllUsers);
+app.post("/user/v1/users/:userId", getUserById);
+
+app.listen(PORT, () => {
+    console.log(`Running on Server PORT ${PORT}`);
 });
